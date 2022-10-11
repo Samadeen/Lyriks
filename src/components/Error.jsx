b0413e7e0d76b397361af0a7dcb76d756d2c1c29
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-10-11 21:08:24 +0100

    Error Component

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
-
+/* eslint-disable jsx-quotes */
 const Error = () => (
-  <div>Error</div>
+  <div className='w-full justify-center items-center'>
+    <h1 className='font-bold text-2xl text-white mt-2'>
+      Something went wrong. Please try again
+    </h1>
+  </div>
 );
 
 export default Error;
